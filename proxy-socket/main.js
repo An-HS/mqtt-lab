2e8632edd99d072690a417c650a7ac206ae90960
An-HS <zxc5894265@gmail.com>
2022-07-13 18:13:00 +0800

    feat: :sparkles: finish homework

--- proxy-socket/main.js
+++ proxy-socket/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 var cors = require("cors");
 var mqtt = require("mqtt");
+const { CLIENT_RENEG_LIMIT } = require("tls");
 const app = express();
 
 var serverPort = 8282;
@@ -25,22 +26,54 @@ server.listen(serverPort, () => {
   console.log("ssh websocket server started");
 });
 
+
 /*
 mqtt init
 */ 
 
+// mqtt 的基本設定
+//var client = mqtt.connect("mqtt://localhost:1883");
+var client = mqtt.connect("mqtt://localhost:1883", {
+  username: "an",
+  password: "1111",
+});
+
+// 建立 mqtt 連線並 subscribe topic "test"
+client.on("connect", function () {
+  console.log("MQTT CONNECTION START");
+  client.subscribe("monosparta/chat");
+});
+
+/*// 設定接收訊息的函數, { qos: 0 }
+client.on("message", function (topic, message) {
+  console.log(message.toString())
+});*/
+
+
 const io = require("socket.io")(server);
 
 io.on("connection", function (socket) {
+  //console.log("user connect")
   socket.on("disconnect", function () {
     
   });
   socket.on("chat", function (msg) {
-    //
+    // 從 client 收到訊息時，使用 topic "test" 傳送訊息到 mqtt broker
+    //console.log(msg);
+    client.publish("monosparta/chat", JSON.stringify(msg));
   });
-  try {
-    //
+  /*try {
+    //以 test 發送訊息給監聽的 client 
+    socket.emit(topic, JSON.parse(msg));
   } catch {
     socket.emit("data", "Node not found.");
-  }
+  }*/
+  client.on("message", function (topic, message) {
+    socket.emit("chat", JSON.parse(message));
+    //console.log(message.toString());
+  });
 });
+
+
+
+
--- python-mqtt/main.py
+++ python-mqtt/main.py
@@ -1,3 +1,4 @@
+from matplotlib.font_manager import json_load
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import json
@@ -6,8 +7,8 @@ from datetime import datetime
 host= "localhost"
 port= 1883
 keepalive= 60
-username= "admin"
-password= "admin"
+username= "an"
+password= "1111"
 subtopic= "monosparta/chat"
 
 # mqtt subscribe
@@ -18,3 +19,36 @@ subtopic= "monosparta/chat"
 
 # mptt init
 
+
+# 建立連線的函數
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    # 每次連線之後，設定訂閱主題
+    client.subscribe(subtopic, qos=0)
+
+# 接收訊息的函數
+def on_message(client, userdata, msg):
+    #print(f"[{msg.topic}]: {msg.payload}")
+    msg_load = json.loads(msg.payload)
+    with open("log.txt","a", encoding="utf-8") as f:
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        f.write(F"[{time}]{msg_load['name']}:{msg_load['message']}\n")
+
+# 建立 MQTT Client 物件
+client = mqtt.Client()
+
+# 設定建立連線的函數
+client.on_connect = on_connect
+
+# 設定接收訊息的函數
+client.on_message = on_message
+
+# 設定登入帳號密碼（若無則可省略）
+client.username_pw_set(username,password)
+
+# 連線至 MQTT 伺服器（伺服器位址,連接埠）
+client.connect(host, port)
+
+# 進入無窮處理迴圈
+client.loop_forever()
